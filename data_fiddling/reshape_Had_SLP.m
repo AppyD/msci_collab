eedbadb5b1dbfd565322b260396bf3d9efb8136b
AppyD <apurva.deshpande@gmail.com>
2016-12-02 17:33:09 +0000

    added new file
    function to reformat the hadley SLP2 data for 1850-2005 into a matrix of
    the right dimensions

---
+++ data_fiddling/reshape_Had_SLP.m
@@ -0,0 +1,15 @@
+function reshaped_data = reshape_Had_SLP(data_matrix)
+    
+    num_rows = 37;    %37 different latitude values
+    num_cols = size(data_matrix,2);
+    num_months = size(data_matrix,1)/num_rows;
+    
+    reshaped_data = zeros(num_rows,num_cols,num_months);
+    page = 0;
+    
+    for i = 1:37:size(data_matrix,1)
+        page = page + 1;
+        reshaped_data(:,:,page) = data_matrix(i:i+36,:);
+    end
+    
+end
